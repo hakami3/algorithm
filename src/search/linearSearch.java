44eb10c76590048562ea5502b4cfab5183b5b4e0
hakami3 <44500993+hakami3@users.noreply.github.com>
2021-02-09 09:58:37 +0900

    Create linearSearch.java

---
+++ src/search/linearSearch.java
@@ -0,0 +1,26 @@
+package search;
+
+public class linearSearch {
+	 public static int search(int arr[], int x) 
+	    { 
+	        int n = arr.length; 
+	        for (int i = 0; i < n; i++)  
+	        { 
+	            if (arr[i] == x) 
+	                return i; 
+	        } 
+	        return -1; 
+	    } 
+	  
+	    public static void main(String args[]) 
+	    { 
+	        int arr[] = { 5, 3, 7, 10, 40, 100}; 
+	        int x = 10; 
+	  	    
+	        int result = search(arr, x); 
+	        if (result == -1) 
+	            System.out.print("Element is not present in array"); 
+	        else
+	            System.out.print("Element is present at index "+ result); 
+	    } 
+}
